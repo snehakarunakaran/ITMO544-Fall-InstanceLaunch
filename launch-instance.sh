9204b4bff6141df0d166b51001423705ff825823
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-10-03 16:20:07 -0500

    Added variables in place of arguments

--- launch-instance.sh
+++ launch-instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-d05e75b8 --count 2 --instance-type t2.micro --key-name ITMO544-Fall2015-VirtualBox --security-group-id sg-18b4bc7f --subnet-id subnet-5e540975 --associate-public-ip-address --user-data file://install-webserver.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-id $5 --subnet-id $6 --associate-public-ip-address --user-data file://../ITMO544-Fall-EnvSetup/install-webserver.sh --debug
