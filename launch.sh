70dfd1d73243267fdd0c4f11109362960701a96a
snehakarunakaran <skaruna1@hawk.iit.edu>
2015-10-03 22:55:58 -0500

    Changed the order of the arguments

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-id $5 --subnet-id $6 --associate-public-ip-address --user-data file://../ITMO544-Fall-EnvSetup/install-env.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-id $4 --subnet-id $5 --associate-public-ip-address --user-data file://../ITMO544-Fall-EnvSetup/install-env.sh --debug
